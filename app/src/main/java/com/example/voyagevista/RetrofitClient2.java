f9ee6f8dbdd2b280ee770af3a835eb352cef4c13
chabchi02 <71357166+chabchi02@users.noreply.github.com>
2023-10-23 09:08:16 -0400

    - unused variables

--- app/src/main/java/com/example/voyagevista/RetrofitClient2.java
+++ app/src/main/java/com/example/voyagevista/RetrofitClient2.java
@@ -3,8 +3,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitClient2 {
     private static Retrofit retrofit;
-    static String lat = userinfo.Lat;
-    static String lon = userinfo.Long;
     private static String BASE_URL = "https://api.bigdatacloud.net/";
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
